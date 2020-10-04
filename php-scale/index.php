ec855b86d6932fe3cfa3c5571cd88962dad123d4
Student User <student@workstation.lab.example.com>
2020-10-04 02:07:32 -0400

    modified to original

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "This is version 3 of the app modified by Hari. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is version 1 of the app modified to original. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
